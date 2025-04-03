553ec18d720cc9048a41c73996f799491ac4250f
Chaman Haldar🎧 <chamanhaldar93@gmail.com>
2025-04-03 10:48:25 +0530

    v1-c

--- server.js
+++ server.js
@@ -1,21 +1,16 @@
 const express = require("express");
-const app = express();
-const path = require("path");
-const ejsMate = require("ejs-mate");
-app.use(express.static(path.join(__dirname,"views"))); 
-app.use(express.static(path.join(__dirname,"assets")));
+const app = express()
 
-app.set("views engine","ejs");
-app.engine("ejs", ejsMate);
-app.use(express.static(path.join(__dirname,"/public")));
-
-app.get("/",(req,res)=>{
-    res.render("login.ejs");
-});
+app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 
-app.listen(8080,()=>{
-    console.log("server is running on port 5050");
-});
+app.get('/', (req,res)=>{
+    res.send('Hello');
+})
 
 
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+  });
