5aaa9f28712bbd66de98bd825247a0ff29ffe9bf
soham <pisesoham19@gmail.com>
2025-04-03 11:38:22 +0530

    -s

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const ejsMate = require("ejs-mate");
+// const ejsMate = require("ejs-mate");
 app.use(express.static(path.join(__dirname,"views"))); 
 app.use(express.static(path.join(__dirname,"assets")));
 
-app.set("views engine","ejs");
-app.engine("ejs", ejsMate);
+// app.set("views engine","ejs");
+// app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname,"/public")));
 
 app.get("/",(req,res)=>{
