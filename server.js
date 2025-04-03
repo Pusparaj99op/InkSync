bfba07a06cf8d0f1b9a7f1a836e72009cf62291e
soham <pisesoham19@gmail.com>
2025-04-03 11:48:19 +0530

    -s

--- server.js
+++ server.js
@@ -1,19 +1,4 @@
 const express = require("express");
-<<<<<<< HEAD
-const app = express();
-const path = require("path");
-// const ejsMate = require("ejs-mate");
-app.use(express.static(path.join(__dirname,"views"))); 
-app.use(express.static(path.join(__dirname,"assets")));
-
-// app.set("views engine","ejs");
-// app.engine("ejs", ejsMate);
-app.use(express.static(path.join(__dirname,"/public")));
-
-app.get("/",(req,res)=>{
-    res.render("login.ejs");
-});
-=======
 const app = express()
 const dotenv = require('dotenv');
 dotenv.config();
@@ -23,7 +8,6 @@ connectToDB();
 app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
->>>>>>> cc6abd43547515bc51a3e29d395c9fcd5a51bfac
 
 
 app.get('/', (req,res)=>{
